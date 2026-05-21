069ff0984fd9650b2e5938cdd86110a63534cb2b
Kly-kly <kleinople60@gmail.com>
2026-05-21 11:54:31 +0800

    Add full authentication routes

--- server.js
+++ server.js
@@ -2,17 +2,6 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 4000;
 
-// Log all errors
-process.on('uncaughtException', (err) => {
-  console.error('UNCAUGHT EXCEPTION:', err);
-});
-
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('UNHANDLED REJECTION:', reason);
-});
-
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-}).on('error', (err) => {
-  console.error('SERVER ERROR:', err);
 });
